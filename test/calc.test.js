f4eadaddc5f6d16337b2722ed8053aa315270292
fuziwang <2622860598@qq.com>
2019-03-27 17:17:22 +0800

    rectangel -v0.1-validate

--- test/calc.test.js
+++ test/calc.test.js
@@ -1,6 +1,6 @@
 var expect = chai.expect;
 
-describe('矩形面积函数的测试', function() {
+describe('测试用例', function() {
   it('area(10, 5) = 50', function() {
     var r = rectangle();
     expect(r.area(10, 5)).to.be.equal(50);          
@@ -8,7 +8,17 @@ describe('矩形面积函数的测试', function() {
 
   it('area("10", "5") = 50', function() {
     var r = rectangle();
-    expect(r.area("10", "5")).to.be.equal(50);          
+    expect(r.area('10', '5')).to.be.equal(50);          
   });
 
+  it('valid(-2) = {isOk:false,reason:"不能为负数"}',function(){
+    expect(valid(-2)).to.be.deep.equal({isOk:false,reason:'不能为负数'});
+  });
+  it('valid("abc") = {isOk:false,reason:"不是合法数字"}',function(){
+    expect(valid('abc')).to.be.deep.equal({isOk:false,reason:'不是合法数字'});
+  });
+  it('valid("") = {isOk:false,reason:"不能为空"}',function(){
+    expect(valid('')).to.be.deep.equal({isOk:false,reason:'不能为空'});
+  });
 });
+
