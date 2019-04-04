74b84d5392638804c30ff86dd0be5e6e2192c31b
fuziwang <2622860598@qq.com>
2019-04-04 22:21:18 +0800

    rectangel -v0.1-char-validation

--- calc.js
+++ calc.js
@@ -33,3 +33,27 @@ function valid(data){
   result.isOk = true;
   return result;
 }
+
+function isLegalKey(key,content,pos){
+  if(/[abcdf-zABCDF-Z`~!@#$%^&*()\-=_+[\]{}|;:'",<>/?\\]/.test(key)){
+    return false;
+  }
+  
+  if(key === '.'){
+    if(pos === 0 || content.indexOf('.') !== -1) return false;
+    if(pos === 1 && content.substring(0,1) === '-') return false;
+  }
+  
+  if(key === 'e'){
+    if(pos === 0 || content.indexOf('e')!== -1 
+         || content.indexOf('E')!==-1) return false;
+    if(pos === 1 && content.substring(0,1) === '-') return false;
+  }
+
+  if(key === 'E'){
+    if(pos === 0 || content.indexOf('e')!== -1 
+         || content.indexOf('E')!==-1) return false;
+    if(pos === 1 && content.substring(0,1) === '-') return false;
+  }
+  return true;
+}
