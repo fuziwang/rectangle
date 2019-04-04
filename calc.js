828ea8f714787c6a31d15628a4eabdc0cbf6a804
fuziwang <2622860598@qq.com>
2019-04-04 22:50:41 +0800

    rectangel -v0.1-h5-validation

--- calc.js
+++ calc.js
@@ -8,28 +8,3 @@ function rectangle(){
     }
   };
 }
-
-function valid(data){
-  var result = {
-    isOk:false,
-    reason:''
-  };
-  
-  if(data === ''){
-    result.reason = '不能为空';
-    return result;
-  }
-
-  if(Number(data) < 0){
-    result.reason = '不能为负数';
-    return result;
-  }
-
-  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data)){
-    result.reason = '不是合法数字';
-    return result;
-  }
-
-  result.isOk = true;
-  return result;
-}
--- rectangle.js
+++ rectangle.js
@@ -2,64 +2,19 @@
 $(function(){
   var $width = $('#width'),
       $height = $('#height'),
-      $btnCal = $('#calculate'),
+      $form = $('form'),
       $perimeter = $('#perimeter'),
       $area = $('#area'),
       $widthValidation = $('#width-validation'),
       $heightValidation = $('#height-validation');
 
-  $width.focusout(function(){
-    var w = $width.val();
-
-    var result = valid(w);
-    if(!result.isOk){
-      $widthValidation.html('宽度' + result.reason);
-      $width.select();
-      return;
-    }
-    $widthValidation.html('');
-  });
-
-  $height.focusout(function(){
-    var h = $height.val();
-
-    var result = valid(h);
-    if(!result.isOk){
-      $heightValidation.html('高度' + result.reason);
-      $height.select();
-      return;
-    } 
-    $heightValidation.html('');
-  });
-  $btnCal.click(function(){
-
+  $form.submit(function(e){
     var w = $width.val(),
         h = $height.val();
     
-    var widthResult = valid(w);
-    var heightResult = valid(h);
-    
-    if(!widthResult.isOk){
-      $widthValidation.html('宽度' + widthResult.reason);
-      $width.select();
-      return;
-    }
-
-    if(!heightResult.isOk){
-      $heightValidation.html('高度' + heightResult.reason);
-      $height.select();
-      return;
-    }
-
-    $widthValidation.html('');
-    $heightValidation.html('');
-
     var rect = rectangle();
     $perimeter.val(rect.perimeter(w,h));
     $area.val(rect.area(w,h));
-  });
-
-  $btnCal.mousedown(function(e){// 当点击事件和失去焦点事件冲突的时候触发
     e.preventDefault();
   });
 });
